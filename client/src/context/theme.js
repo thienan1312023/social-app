f06bc39849d548add40919b576ed15d1119cd323
thienan1312023 <thienan2195@gmail.com>
2021-10-19 20:38:41 +0700

    remove codes unnecessary

--- client/src/context/theme.js
+++ client/src/context/theme.js
@@ -1,27 +1,3 @@
-// import React, { useState, createContext } from 'react';
-
-// export const ThemeCon
-text = createContext();
-
-// export const ThemeProvider = ({ children }) => {
-//   const [theme, setTheme] = useState('light');
-
-//   const setThemeColor = (color) => {
-//     setTheme(color);
-//   };
-
-//   return (
-//     <ThemeContext.Provider
-//       value={{
-//         themeColor: theme,
-//         setThemeColor,
-//       }}
-//     >
-//       {children}
-//     </ThemeContext.Provider>
-//   );
-// };
-
 import React, { useState, createContext } from 'react';
 
 export const ThemeContext = createContext();
